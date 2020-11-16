5ecad7ae9f5ab5cff066f71132e2bcd47ea3ce70
z.dansevicius <z.dansevicius@gmail.com>
2020-11-16 10:20:41 +0200

    pirma uzduotis

--- js/main.js
+++ js/main.js
@@ -1,1 +1,78 @@
 console.log(889234);
+// Lyginant, jei rezultatas tenkina palyginimo sąlygą, tai į console išvesti žodį “Pomidoras”, o jei sąlyga nėra tenkinama, išvesti sakinį “Bandykite kitą kartą.”.
+
+// Tarpusavyje palyginti skaičiaus tipo kintamuosius:
+// kuris didesnis
+
+
+// const x = 5;
+// const y = 8;
+// const tiesa ='pomidoras';
+// const netiesa ='Bandykite kitą kartą.'
+
+// if (x < y) {
+//     console.log(tiesa);
+    
+// } else { 
+//     console.log(netiesa);
+// }
+
+// kuris mažesnis
+
+// const x = 5;
+// const y = 8;
+// const tiesa ='pomidoras';
+// const netiesa ='Bandykite kitą kartą.'
+
+
+// if (x < y) {
+//     console.log(tiesa);
+// } else {
+//     console.log(netiesa);
+//  }
+
+
+// ar jie lygūs
+
+// const x = 13;
+// const y = 13;
+// const tiesa ='pomidoras';
+// const netiesa ='Bandykite kitą kartą.'
+
+// if (x == y) {
+//     console.log(tiesa);
+
+// } else {
+//     console.log(netiesa);
+// }
+
+// ar jie nelygūs
+
+// const x = 7;
+// const y = 3;
+// const tiesa ='pomidoras';
+// const netiesa ='Bandykite kitą kartą.'
+
+// if (x !== y) {
+//     console.log(tiesa);
+    
+// } else {
+//     console.log(netiesa);
+    
+// }
+
+// kuris didesnis arba lygus
+
+
+// const x = 7;
+// const y = 7;
+// const tiesa ='pomidoras';
+// const netiesa ='Bandykite kitą kartą.'
+
+// if (x <= y) {
+//     console.log(tiesa);
+    
+// } else {
+//     console.log(netiesa);
+    
+// }
